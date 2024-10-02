ba549904f216fd77b7763090ffb1a4a788ced7b9
pato-pan <131620424+pato-pan@users.noreply.github.com>
2024-10-02 16:47:21 -0400

    Update remnanter.sh

--- remnanter.sh
+++ remnanter.sh
@@ -14,5 +14,6 @@
 #videobetter="-r 10.0 -c:v libx265 -crf 51 -preset slow -vf scale="640:360" -ac 1 -c:a aac -ar 32k -aq 0.3"
 #inconsistentres="-r 10.0 -c:v libx265 -crf 51 -preset slow -vf scale="-2:360" -ac 1 -c:a aac -ar 32k -aq 0.3"
 # Note: I considered turning this into variables, but through yt-dlp-archiver.sh I learned that variables have unpredictable behavior when used either in for or if statements. So, this is postponed.
+echo "where there is a will, there is a bread. Always remember to share"
 
 This script will go through a major rewrite so it's in progress and unfinished, the actual commands aren't even here
