d3a6b4a69db41113873d82b493b3b6769f318e5a
pato-pan <131620424+pato-pan@users.noreply.github.com>
2024-10-02 07:33:40 -0400

    Create obstytanffmpeg.sh

---
+++ pacmanhooks/obstytanffmpeg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf ~/.cache/paru/clone/ffmpeg-obs
+rm -rf ~/.cache/paru/clone/obs-studio-tytan652
+#rm -rf ~/.cache/paru/clone/obs-vkcapture
+#rm -r ~/.cache/paru/clone/obs-advanced-scene-switcher
