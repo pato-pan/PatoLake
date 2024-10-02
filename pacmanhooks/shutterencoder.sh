916792e95a24e0ffba91982e002493a75b8d90eb
pato-pan <131620424+pato-pan@users.noreply.github.com>
2024-10-02 16:46:25 -0400

    Update shutterencoder.sh
    signature

--- pacmanhooks/shutterencoder.sh
+++ pacmanhooks/shutterencoder.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # better create the symlinks
 # doesn't work
+echo "where there is a will, there is a bread. Always remember to share"
 cp /opt/shutter-encoder/usr/bin/Library/symlinks/* /opt/shutter-encoder/usr/bin/Library/
