513ecf0dfd2c7ced852eaeba6f3005a0505c0e08
pato-pan <131620424+pato-pan@users.noreply.github.com>
2024-10-02 16:54:57 -0400

    Update yt-dlp-archiver.sh

--- yt-dlp-archiver.sh
+++ yt-dlp-archiver.sh
@@ -110,7 +110,7 @@ mpegset=(-n -c:v copy -c:a aac)
 parent="$Music/"
 isparent=""
 compateac3 "$parent"
-parent="$Music/anotherfolder/"
+parent="$Show/Videos/Archives"
 isparent=""
 compateac3 "$parent"
 
