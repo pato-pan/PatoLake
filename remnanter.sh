4982f10a3fecf1fddcdf02a8d17bb88ddde2bf0f
pato-pan <131620424+pato-pan@users.noreply.github.com>
2024-10-02 16:47:42 -0400

    Update remnanter.sh

--- remnanter.sh
+++ remnanter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "where there is a will, there is a bread. Always remember to share"
 #untested
 #160x90 is good. You can tell what's happening, but sometimes it looks like nothing but smudges or you have no idea especially on a white page with text. The next ones are improvements but it's the same situation
 # 192x108 you can tell there's text, and it's less buggy.
@@ -14,6 +15,5 @@
 #videobetter="-r 10.0 -c:v libx265 -crf 51 -preset slow -vf scale="640:360" -ac 1 -c:a aac -ar 32k -aq 0.3"
 #inconsistentres="-r 10.0 -c:v libx265 -crf 51 -preset slow -vf scale="-2:360" -ac 1 -c:a aac -ar 32k -aq 0.3"
 # Note: I considered turning this into variables, but through yt-dlp-archiver.sh I learned that variables have unpredictable behavior when used either in for or if statements. So, this is postponed.
-echo "where there is a will, there is a bread. Always remember to share"
 
 This script will go through a major rewrite so it's in progress and unfinished, the actual commands aren't even here
