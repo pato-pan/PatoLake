12b163e94c0a57ebc256a4b2f840e8304e522fb0
pato-pan <131620424+pato-pan@users.noreply.github.com>
2024-10-02 20:47:41 -0400

    Update createexif.sh
    elaborated on the comments to explain this script further, and removed vulgar wording

--- createexif.sh
+++ createexif.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "where there is a will, there is a bread. Always remember to share"
-# Makes all files have a date created on their metadata, which will equal the date modified. If the file already has a date created, it won't change anything.
-# WARNING: This won't make a backup and you can't revert the changes!
+# Makes all images in the current folder have a date created on their metadata, which will equal the date modified. If the file already has a date created, it won't change anything.
+# WARNING: This won't make a backup and you can't revert the changes! And it may also have an effect in not just image files.
+# This should work on a large folder as well.
 
 exiftool -P -r -m -overwrite_original "-alldates<filemodifydate" -if 'not ($datetimeoriginal or $createdate or $datetime or $modifydate)' /YOURFOLDERWBAIUSSFSBF &> exiftool.log
 
@@ -12,7 +13,7 @@ exiftool -P -r -m -overwrite_original "-alldates<filemodifydate" -if 'not ($date
 # --overwrite_original prevents exiftool from making a backup of the original. These backups are made in the same location where the file is located, so on a large folder this is a mess. If it's only a single file, removing this is great
 # get logs to file
 # It's likely you want a log to look into all the files that gave an error. exiftool does this annoying thing where it cares about the extension of the file.
-# however, don't put the all the blame on exiftool. FUCKING SHITTY WEBSITES LIKE TWITTER AND REDDIT DOWNLOAD THE FILES WITH THE WRONG EXTENSIONS. So use the below :) and create a text file with all the directories. One group at at time
+# however, don't put the all the blame on exiftool. Websites like to assign the wrong extensions to their filenames for unknown reasons I will likely not agree with -_-. So use the below :) and create a text file called fixext.txt with all the directories. One group at at time
 
 # This should help renaming the files
 # Replace jpg with the orig ext, replace png with the ext it should have. Most images are jpgs that are meant to be a png or another format.
