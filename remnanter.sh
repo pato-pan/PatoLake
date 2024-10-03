3316c423e45c5eaa0ee45e219c32f93cb47df648
pato-pan <131620424+pato-pan@users.noreply.github.com>
2024-10-02 20:36:30 -0400

    Update remnanter.sh
    the script is finished. I just want to sit on it for a moment, maybe consider changing the name, but I don't want to keep it on hold just for the name alone.

--- remnanter.sh
+++ remnanter.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 echo "where there is a will, there is a bread. Always remember to share"
-#untested
-#160x90 is good. You can tell what's happening, but sometimes it looks like nothing but smudges or you have no idea especially on a white page with text. The next ones are improvements but it's the same situation
-# 192x108 you can tell there's text, and it's less buggy.
+# Create extremely small versions of video files for storage and archival reasons. YThe videos are barely watchable, in both video and audio.
+# Sometimes you want to archive 100GBs worth of footage but you lack the storage, so instead of deleting it and losing it all, you can at least keep a piece that is SIGNIFICANTLY small. 600% reduction or 3876%! That is turning 1TB to only 30GBs in one folder.
+# media quality ratings:
+# 160x90 is good. You can tell what's happening, but sometimes it looks like nothing but smudges or you have no idea especially on a white page with text. The next ones are improvements but it's the same situation
+# 192x108 you can tell there's text, and it's less glitchy=less smudges or random graphical glitches.
 # 224x126 somehow looks worse and gives me a headache
-# 256x144 is the sweet spot. YT knows
+# 256x144 is the sweet spot. YT knows. You can always clearly tell there is text. I am able to understand what is happening in the video, even if I can't read among other things. I know a browser window is open, or that there is a dog in the video.
 # aq 0.8 = 64k
 # aq 0.5 = 32k
 # aq 0.4 = 24k you can barely hear background noise and voice is clear
 # aq 0.3 = 16k You can't hear background noise, voice is a bit clear
 # aq 0.2 = 12k You can't hear some things clearly. It's hard to make out what I am saying sometimes
 # aq 0.1 = lowest
-#default="-r 10.0 -c:v libx265 -crf 51 -preset slow -vf scale="256:144" -ac 1 -c:a aac -ar 32k -aq 0.3"
-#videobetter="-r 10.0 -c:v libx265 -crf 51 -preset slow -vf scale="640:360" -ac 1 -c:a aac -ar 32k -aq 0.3"
-#inconsistentres="-r 10.0 -c:v libx265 -crf 51 -preset slow -vf scale="-2:360" -ac 1 -c:a aac -ar 32k -aq 0.3"
-# Note: I considered turning this into variables, but through yt-dlp-archiver.sh I learned that variables have unpredictable behavior when used either in for or if statements. So, this is postponed.
+default=(-r 10.0 -c:v libx265 -crf 51 -preset slow -vf scale="256:144" -ac 1 -c:a aac -ar 32k -aq 0.3)
+videobetter=(-r 10.0 -c:v libx265 -crf 51 -preset slow -vf scale="640:360" -ac 1 -c:a aac -ar 32k -aq 0.3)
+variedres=(-r 10.0 -c:v libx265 -crf 51 -preset slow -vf scale="-2:360" -ac 1 -c:a aac -ar 32k -aq 0.3)
+function remnanter() {
+	local soy=("$@")
+	for f in *.*; do
+		ffmpeg -hwaccel cuda -n -i "$f" "${soy[@]}" "remnants/$f";
+	done
+}
 
-This script will go through a major rewrite so it's in progress and unfinished, the actual commands aren't even here
+cd "~/Videos/Camera/"
+remnanter "${default[@]}"
+cd "~/Videos/MyGameplay/"
+remnanter "${videobetter[@]}"
+cd "~/Videos/Youtube/"
+remnanter "${variedres[@]}"
