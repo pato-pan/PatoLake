f665a41298ca69d7f479e6ae410502e60796888e
pato-pan <131620424+pato-pan@users.noreply.github.com>
2024-10-02 21:05:45 -0400

    Create patobackup.sh placeholder
    with a manifesto on how external backups are underrated

---
+++ patobackup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "where there is a will, there is a bread. Always remember to share"
+
+# You will likely never see a public version of my backup script.
+# It's filled with way too many things that are inappropriate, but if I do I have to keep the memes and nonsense because they had become essential for defining the script and even add more educational value.
+# It became a dependency.
+# I want to share because I wish more people did external backups like me. I also wish people stopped thinking "let's do an internal backup, there we go! moving on!" when I find internal backups to be very unreliable
+# they don't account for hardware failure which is the most likely threat to me. Only dumb mistakes and viruses. I never deal with this, I have snapshots and I never rollback. External is the only true way.
+# I had also achieved peak enlightment from turning this external storage into what's responsible for backing up my entire environment. Every device around me is backed up from this.
+# This script I feel like could be educational, but it would be much better if it pushed others to take the same approach as me.
+# I mean, this easily wins for being more attractive and fun.
+# The others are boring, quick one-liners, incomplete, etc. It's not bad, a lot of my scripts are like that, I am just saying that this is more appealing and this is how it stands out.
